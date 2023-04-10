110e6dae16c5c576893433156302b7dc77e8fd0c
jay <jay@jaymakes11.com>
2023-04-10 14:05:12 +0300

    Improve font handling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts,svg,yaml,astro}'],
-	plugins: [require('@tailwindcss/typography'), require('tailwindcss-font-inter')],
+	theme: {
+		fontFamily: {
+			mono: defaultTheme.fontFamily.mono,
+			serif: defaultTheme.fontFamily.serif,
+			display: defaultTheme.fontFamily.display,
+			sans: [
+				`Inter var, ${defaultTheme.fontFamily.sans}`,
+				{
+					fontFeatureSettings: '"cv11", "ss01"',
+					fontVariationSettings: '"opsz" 32',
+				},
+			],
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
 }
